d7db195e444d09edf274dfdacb767fcd74863e48
Christine Hu <chrhu@paul-f3Ar4s10.clusters.42paris.fr>
2024-04-23 10:15:55 +0200

    add philo4

---
+++ philo 4/srcs/philo.c
@@ -0,0 +1,37 @@
+#include "../includes/philo.h"
+
+//Convert seconds and microseconds to millisecondes
+long long get_time(void) 
+{
+    struct timeval tv;
+
+    if (gettimeofday(&tv, NULL) < 0)
+		printf("%sError function : gettimeofday()%s\n", RED, DEFAULT);
+    return (((long long)tv.tv_sec) * 1000) + ((long long)tv.tv_usec / 1000);
+}
+
+int main(int argc, char *argv[]) 
+{
+	t_data data;
+
+	if (argc == 5 || argc == 6)
+	{
+		if ((check_number(argc,argv) == 1) || (initialize(&data, argv) == 1))
+		{
+			printf("%sError init philo%s", RED, DEFAULT);
+			return (1);
+		}
+		if (data.nb_philo == 1)
+			handle_one_philo(&data);
+		else
+			init_thread(&data);
+	}
+	else
+	{
+        printf("%sPlease enter 5/6arguments :", RED);
+		printf("./philo nb_of_philo time_to_die");
+		printf("time_to_eat time_to_sleep number_of_eat%s\n", DEFAULT);
+        return (1);
+    }
+    return 0;
+}
---
+++ philo 4/srcs/utils.c
@@ -0,0 +1,105 @@
+#include "../includes/philo.h"
+
+int check_number(int argc, char **argv)
+{
+	int i;
+
+	i = 1;
+	while (i < argc)
+	{
+		if (ft_atoi(argv[i]) > 2147483647 || ft_atoi(argv[i]) < 1)
+			return (1);
+		i++;
+	}
+	i = 1;
+	while (i < argc)
+	{
+		if (ft_atoi(argv[i]) < 1)
+			return (1);
+		i++;
+	}
+	if (ft_atoi(argv[1]) > 0)
+		return (0);
+	return (0);
+}
+
+int	ft_atoi(const char *nptr)
+{
+	int	i;
+	int	result;
+
+	i = 0;
+	result = 0;
+	while ((nptr[i] > 8 && nptr[i] < 14) || nptr[i] == ' ')
+		i++;
+	if (nptr[i] == '-')
+		return (-1);
+	if (nptr[i] == '+')
+		i++;
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		result = result * 10 + (nptr[i] - '0');
+		i++;
+	}
+	return (result);
+}
+
+void print_status(t_data *data, int philo, const char *status, int dead) 
+{
+    long long current_time;
+	
+	current_time = get_time();
+	
+	pthread_mutex_lock(&(data->print));
+	if (!(data->philo_dead))
+	{
+    	printf("%lld %d ", current_time - data->start_time, philo + 1);
+		printf("%s\n", status);
+	}
+	else if (dead == 1)
+	{
+		printf("%lld %d ", current_time - data->start_time, philo + 1);
+		printf("%s\n", status);
+	}
+	pthread_mutex_unlock(&(data->print));
+	return ;
+}
+
+int ft_usleep(t_data *data, long long time)
+{
+	long long start;
+	long long current;
+
+	start = get_time();
+	current = get_time();
+
+	while (!(data->philo_dead))
+	{
+		current = get_time();
+		if ((current - start) > time)
+			break;
+		usleep(100);
+	}
+	return (0);
+}
+
+void thread_wait_destroy(t_data *data, t_philo *philo)
+{
+    int i;
+
+	i = data->nb_philo;
+    while (--i >= 0)
+    {
+        if (pthread_join(philo[i].threads, NULL) != 0)
+            return ;
+    }
+	i = data->nb_philo;
+    while (--i >= 0)
+        pthread_mutex_destroy(&data->forks[i]);
+    pthread_mutex_destroy(&data->print);
+	pthread_mutex_destroy(&data->dead_check);
+	pthread_mutex_destroy(&data->meal_check);
+	//add
+	pthread_mutex_destroy(&data->eat_check);
+	pthread_mutex_destroy(&data->lastmeal_check);
+}
